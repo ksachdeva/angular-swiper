e0fc7087593a98cbc905157067789c883143ea2a
Kapil Sachdeva <ksachdeva17@gmail.com>
2015-03-22 18:24:00 -0500

    chore - add the gulp task to generate annotated & minified and copy it in the dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var server = require('gulp-webserver');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
 
 gulp.task('serve', function() {
     gulp.src('./')
@@ -8,4 +10,11 @@ gulp.task('serve', function() {
             directoryListing: true,
             open: true
         }));
+});
+
+gulp.task('dist', function() {
+    return gulp.src('src/angular-swiper.js')
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
