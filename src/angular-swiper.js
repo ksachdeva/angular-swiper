634ff67f17e2b7a1fdf2695b799689beac1a2040
abrbhat <bhatnagarabhiroop@gmail.com>
2015-12-28 17:57:40 +0530

    currentSlide adjusted for loops

--- src/angular-swiper.js
+++ src/angular-swiper.js
@@ -101,17 +101,40 @@
                         swiper = new Swiper($element[0].firstChild, params);
                     }
 
-                    if(!angular.isUndefined($scope.currentSlide)){
+                    if (!angular.isUndefined($scope.currentSlide)){
                       swiper.on('slideChangeEnd', function(){
-                        $timeout(function(){
-                          $scope.currentSlide = Math.abs(swiper.activeIndex);
+                        $timeout (function(){
+                          $scope.internalSlide = true;
+                          if (swiper.params.loop){
+                            var currentSlideElement = $element[0].querySelector('.swiper-slide-active');
+                            var currentSlideIndexString = currentSlideElement.attributes['data-swiper-slide-index'].value;
+                            $scope.currentSlide = parseInt(currentSlideIndexString, 10);
+                          } else {
+                            $scope.currentSlide = Math.abs(swiper.activeIndex);
+                          }
                         });
                       });
                     }
 
                     $scope.$watch('currentSlide', function(newValue, oldValue){
-                      if  ((!angular.isUndefined(newValue)) && (newValue != oldValue)){
-                        swiper.slideTo(newValue);
+                      if ($scope.internalSlide){
+                        $scope.internalSlide = false;
+                        return;
+                      }
+                      if ((!angular.isUndefined(newValue)) && (newValue != oldValue)){
+                        if (swiper.params.loop){
+                          var possibleDestinationIndexes = [];
+                          for (var index = 0; index < swiper.slides.length; index++){
+                            var slide = swiper.slides[index];
+                            var dataIndex = parseInt(slide.attributes['data-swiper-slide-index'].value, 10);
+                            if(dataIndex === newValue){
+                              swiper.slideTo(index);
+                              break;
+                            }
+                          }
+                        } else {
+                          swiper.slideTo(newValue);
+                        }
                       }
                     });
 
