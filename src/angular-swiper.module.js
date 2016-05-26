0f6ecd5a55e648aba9ccfacfb8a9c481f9b43a14
Alfredo de la Calle <alfredo.delacallevillarroya@sopra.com>
2016-05-26 09:28:19 +0200

    [UPD] Split files for better location [ADD] gulp-concat to concatenate files & uglify

---
+++ src/angular-swiper.module.js
@@ -0,0 +1,8 @@
+(function(window, angular, undefined) {
+
+    'use strict';
+
+    angular
+        .module('ksSwiper', []);
+
+})(window, angular, undefined);
---
+++ src/angular-swiper.provider.js
@@ -0,0 +1,13 @@
+(function(window, angular, undefined) {
+
+    'use strict';
+
+    angular
+        .module('ksSwiper')
+        .provider('angularSwiperConfig', angularSwiperConfig);
+
+    function angularSwiperConfig() {
+
+    }
+
+})(window, angular, undefined);
